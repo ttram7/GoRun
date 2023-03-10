78744524975e92a99158c48578afbc29294c8bda
Tina Tram <tinatram07@gmail.com>
2023-03-10 09:16:03 -0600

    changed bar graph color, changed graph title

--- src/components/StatsGraph/StatsGraph.jsx
+++ src/components/StatsGraph/StatsGraph.jsx
@@ -23,9 +23,6 @@ import {
 
 function StatsGraph() {
   const runList = useSelector(store => store.runList);
-  console.log(runList);
-  //const [heading, setHeading] = useState('Functional Component');
-
   const [weeklyRunList, setWeeklyRunList] = useState([]);
 
   const [chartData, setChartData] = useState({
@@ -33,16 +30,10 @@ function StatsGraph() {
   })
 
   const [chartOptions, setChartOptions] = useState({});
+
   useEffect(() => {
     fetchWeeklyRuns();
-    //if (weeklyRunList.length > 0) {
-    //setGraph();
-    //}
   }, [])
-
-  // useEffect(() => {
-  //   setGraph();
-  // }, [])
 
   const fetchWeeklyRuns = () => {
     console.log('in fetch weekly runs')
@@ -50,7 +41,7 @@ function StatsGraph() {
     .then((response) => {
       console.log('response from week stats', response.data);
       setGraph(response.data);
-      setWeeklyRunList(response.data);
+      //setWeeklyRunList(response.data);
       //setChartData(response.data);
       console.log('weekList', response.data)
     })
@@ -58,55 +49,15 @@ function StatsGraph() {
       console.log("error getting week stats", error);
     });
   }
-  
-  // useEffect(() => {
-  //   fetchWeeklyRuns();
-  //   //if (weeklyRunList.length > 0) {
-  //   //setGraph();
-  //   //}
-  // }, [])
-
-  // useEffect(() => {
-  //   setGraph();
-  // }, [])
-
-
-//     if (weeklyRunList.length > 0) {
-//     setChartData({
-//         //labels: 'M, T, W',
-//         //labels:['M', 'T', 'W', 'TH'],
-//         labels:['M', 'T', 'W', 'TH', 'F', 'S', 'S'],
-//         datasets: [
-//           {
-//             label: 'Distance',
-//             data: weeklyRunList.map((run) => run.distance),
-//             //data: chartData.map(run => run.distance),
-//             borderColor: 'rgb(255, 99, 132)',
-//             backgroundColor: 'rgba(255, 99, 132, 0.5)',
-//           }
-//         ]
-//       });
-//       setChartOptions({
-//         responsive: true,
-//         plugins: {
-//           legend: {
-//             position: 'top'
-//           },
-//           title: {
-//             display: true,
-//             text: 'Chart.js Bar Chart',
-//           },
-//         },
-//       });
-      
-// }}, []);
-//}, []);
 
  const setGraph = (weeklyRunList) => {
-  
+    console.log('weeklyRunList', weeklyRunList)
+    const distanceList = weeklyRunList.map((run) => run.distance)
+    console.log('distanceList', distanceList) // output: [3, 3, 2]
+
+
+    
     setChartData({
-        //labels: 'M, T, W',
-        //labels:['M', 'T', 'W', 'TH'],
         labels:['M', 'T', 'W', 'TH', 'F', 'S', 'S'],
         datasets: [
           {
@@ -114,7 +65,7 @@ function StatsGraph() {
             data: weeklyRunList.map((run) => run.distance),
             //data: chartData.map(run => run.distance),
             borderColor: 'rgb(255, 99, 132)',
-            backgroundColor: 'rgba(255, 99, 132, 0.5)',
+            backgroundColor: '#3395ef',
           }
         ]
       });
@@ -126,23 +77,22 @@ function StatsGraph() {
           },
           title: {
             display: true,
-            text: 'Chart.js Bar Chart',
+            text: 'Distances for this week',
           },
         },
       });
-      
 }
  
 return (
   <>
-      {weeklyRunList.length > 0 &&
+      {/* {weeklyRunList.length > 0 && */}
       <div>
         <h3>Stats Graph</h3>
         <div className="bar-chart">
           <Bar options={chartOptions} data={chartData}/>
         </div>
       </div>
-    }
+    {/* } */}
     </>
     );
 
