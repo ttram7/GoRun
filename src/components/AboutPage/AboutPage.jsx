bc67c0c3a55c00f4e724ba358ce574c44e2e29ff
Tina Tram <tinatram07@gmail.com>
2023-03-15 00:42:22 -0500

    added information to about page

--- src/components/AboutPage/AboutPage.jsx
+++ src/components/AboutPage/AboutPage.jsx
@@ -1,6 +1,7 @@
 import React from 'react';
 import LogOutButton from '../LogOutButton/LogOutButton';
 import BottomNav from '../BottomNav/BottomNav';
+import { useHistory } from 'react-router-dom';
 
 // This is one of our simplest components
 // It doesn't have local state,
@@ -8,13 +9,28 @@ import BottomNav from '../BottomNav/BottomNav';
 // or even care what the redux state is'
 
 function AboutPage() {
+  const history = useHistory();
+  const logOut = () => {
+    history.push('/login');
+  }
   return (
     <div className="container">
       <div>
-        <p>This about page is for anyone to read!</p>
+        <p>Technologies used:</p>
+        <ul>
+          <li>React</li>
+          <li>Node</li>
+          <li>Express</li>
+          <li>React Chart js 2</li>
+          <li>Material UI</li>
+          <li>Sweet Alert</li>
+        </ul>
+        <br />
+        <p>Acknowledgements</p>
+        <p>I'd like to thank all the instructors and staff at Prime, my classmates, and my friends and family.</p>
       </div>
       <div>
-      <LogOutButton className="navLink" />
+      {/* <LogOutButton className="navLink" onClick={() => logOut()} /> */}
       </div>
       <BottomNav />
     </div>
